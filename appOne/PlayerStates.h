688fca5fe56e4b5bf978ad703c569c647a6bc152
Coding-Ocean <eijo524@gmail.com>
2022-02-25 16:46:21 +0900

    tidy State

--- appOne/PlayerStates.h
+++ appOne/PlayerStates.h
@@ -1,70 +1,41 @@
 #pragma once
 #include "State.h"
-//------------------------------------------------------
+//-----------------------------------------------------------------------------
 class PlayerWait : public State
 {
 public:
-	PlayerWait(class StateComponent* OwnerCompo)
-		:State(OwnerCompo)
-	{ }
-
+	PlayerWait(class StateComponent* OwnerCompo) :State(OwnerCompo) { }
+	const char* GetName() const override { return "Wait"; }
 	void OnEnter() override;
 	void Update() override;
-
-	const char* GetName() const override
-	{
-		return "Wait";
-	}
 };
-//------------------------------------------------------
+//-----------------------------------------------------------------------------
 class PlayerWalk : public State
 {
 public:
-	PlayerWalk(class StateComponent* OwnerCompo)
-		:State(OwnerCompo)
-	{ }
-
-	// Override with behaviors for this state
+	PlayerWalk(class StateComponent* OwnerCompo) :State(OwnerCompo) { }
+	const char* GetName() const override { return "Walk"; }
 	void OnEnter() override;
 	void Update() override;
-
-	const char* GetName() const override
-	{
-		return "Walk";
-	}
 };
-//------------------------------------------------------
+//-----------------------------------------------------------------------------
 class PlayerJump : public State
 {
 public:
-	PlayerJump(class StateComponent* OwnerCompo)
-		:State(OwnerCompo)
-	{ }
-
+	PlayerJump(class StateComponent* OwnerCompo) :State(OwnerCompo) { }
+	const char* GetName() const override { return "Jump"; }
 	void OnEnter() override;
 	void Update() override;
-
-	const char* GetName() const override
-	{
-		return "Jump";
-	}
 private:
 	float mVel;
 	float mAcc;
 };
-//------------------------------------------------------
+//-----------------------------------------------------------------------------
 class PlayerPunch : public State
 {
 public:
-	PlayerPunch(class StateComponent* OwnerCompo)
-		:State(OwnerCompo)
-	{ }
-
+	PlayerPunch(class StateComponent* OwnerCompo) :State(OwnerCompo) { }
+	const char* GetName() const override { return "Punch"; }
 	void OnEnter() override;
 	void Update() override;
-
-	const char* GetName() const override
-	{
-		return "Punch";
-	}
 };
--- appOne/State.h
+++ appOne/State.h
@@ -3,15 +3,13 @@ class State
 {
 public:
 	State(class StateComponent* OwnerCompo)
-		:mOwnerCompo(OwnerCompo)
-	{
-	}
-	// State-specific behavior
+		:mOwnerCompo(OwnerCompo){}
+	// ステート名のゲッター
+	virtual const char* GetName() const = 0;
+	// ステート固有の動き
 	virtual void Update() = 0;
 	virtual void OnEnter() {};
 	virtual void OnExit() {};
-	// Getter for string name of state
-	virtual const char* GetName() const = 0;
 protected:
 	class StateComponent* mOwnerCompo;
 };
